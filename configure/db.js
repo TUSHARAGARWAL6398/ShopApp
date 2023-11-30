4a64bd39d1d431036b37fb901e30c84dd589bd27
Tushar Agarwal <106472121+TUSHARAGARWAL6398@users.noreply.github.com>
2023-11-30 11:34:15 +0530

    Update db.js

--- configure/db.js
+++ configure/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbConnect = () => mongoose.connect('mongodb://127.0.0.1:27017/shopApp').then(() => {
+const dbConnect = () => mongoose.connect('mongodb+srv://tusharagarwalcs21:lpbxPkQ9QoET4v6Z@cluster0.oibzvzj.mongodb.net/?retryWrites=true&w=majority').then(() => {
     console.log("Connecting Database");
 }).catch((err) => {
     console.log(err);
